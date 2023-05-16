792c81da19f102b74f69af86c496425b781e4982
michacp <67809880+michacp@users.noreply.github.com>
2023-05-16 10:46:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
+const url='mongodb://localhost/mongodb_sincro'
+mongoose.connect(url,{
+
+})
+.then(()=> console.log('conectado a mongo'))
+.catch((e)=> console.log('el error de conexion es : '+e))
+
+const brandschema= mongoose.Schema({
+id:Number,
+name:String,
+age:Number
+})
+
+const brandmodel=mongoose.model('brand',brandschema)
+
+//mostrar
+
+const mostrar=async()=>{
+    console.log('sss')
+  const brands=  await brandmodel.find()
+  console.log(brands)
+}
+
+const crear=async()=>{
+const brands=new brandmodel({
+  id:3,
+name:'aseee',
+age:223
+})
+const resultado=await brands.save()
+console.log(resultado)
+}
+
+crear()
+//mostrar()
